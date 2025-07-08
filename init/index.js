bff18c19a6f47a94edd2d80d686d34c4efedea3c
mduzairshams <mduzairshams@gmail.com>
2025-07-08 18:34:46 +0530

    update

--- init/index.js
+++ init/index.js
@@ -19,8 +19,12 @@ const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: '68656d69f6d3d9df9ee23e66'
-  }))
+    owner: '68656d69f6d3d9df9ee23e66',
+    geometry: {
+      type: 'Point',
+      coordinates: [0, 0], // Default coordinates, can be updated as needed
+    },
+  })) 
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
